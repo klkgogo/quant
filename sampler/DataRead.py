bda9bb54a7baf993f01b715cd7e377eb5c930c79
henrykong <49072565@qq.com>
2018-05-14 20:01:50 +0800

    DataRead示例

--- sampler/DataRead.py
+++ sampler/DataRead.py
@@ -2,10 +2,11 @@ import pandas as pd
 import time
 start = time.clock()
 store = pd.HDFStore('ticker_26.h5', 'r')
-#data = store.select("data", where=('code=="HK.00700"'))
-data = store.select("data", where=('price > 4'))
-print(data)
+data = store.select("data")
+# data = store.select("data", where=('price > 4'))
 end = time.clock()
 delta = end - start
 print("time: ", delta)
+
+
 
